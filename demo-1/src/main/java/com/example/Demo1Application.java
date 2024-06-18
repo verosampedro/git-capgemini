73777a406bca5ea77cce8a04d90e5e6cf36e2254
verosampedro <verosampedro@icloud.com>
2024-06-18 11:16:25 +0200

    creating a maven project to the repository

---
+++ demo-1/src/main/java/com/example/Demo1Application.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.ioc.Saluda;
+
+@SpringBootApplication
+public class Demo1Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+
+	@Autowired
+	Saluda saluda;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		System.err.println("Aplicación arrancada...");
+//		var saluda = new Saluda();
+		saluda.saluda("Mundo");
+	}
+
+}
