a938ea6b76f6a19419cccb7511d1a4c1f696d818
verosampedro <verosampedro@icloud.com>
2024-06-27 13:16:22 +0200

    configuration personas batch project

---
+++ lotes-java/src/main/java/com/example/models/Persona.java
@@ -0,0 +1,13 @@
+package com.example.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class Persona {
+
+	private long id;
+	private String nombre;
+	private String correo;
+	private String ip;
+}
