29d85032d92dfc40e8d8d4775fd4d00f0db020cf
verosampedro <verosampedro@icloud.com>
2024-07-11 13:34:27 +0200

    updating demos component

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoggerService } from '@my/core';
 import { NotificationComponent, NotificationModalComponent } from './main';
-import { DemosComponent } from './ejemplos/demos/demos.component';
+import { DemosComponent } from './ejemplos';
+import { NotificationService, NotificationType } from './common-services';
+import { HomeComponent } from "./main/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NotificationComponent, NotificationModalComponent, DemosComponent],
+  imports: [RouterOutlet, NotificationComponent, NotificationModalComponent, DemosComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title: string = 'World';
+
 }
--- angular/src/app/ejemplos/demos/demos.component.ts
+++ angular/src/app/ejemplos/demos/demos.component.ts
@@ -10,7 +10,55 @@ import { NotificationService, NotificationType } from 'src/app/common-services';
   styleUrl: './demos.component.css'
 })
 export class DemosComponent implements OnInit, OnDestroy  {
+  private name: string = 'world'
+  date='2024-07-11'
+  fontSize = 24
+  list = [
+    { id: 1, name: 'Madrid'},
+    { id: 2, name: 'BARCELONA'},
+    { id: 3, name: 'oviedo'},
+    { id: 4, name: 'ciudad Real'},
+  ]
+  idProvince = 2
+
+  result?: string
+  visible = true
+  esthetic = { important: true, error: false, urgent: true }
+Component: any;
+
   constructor(public vm: NotificationService) { }
+
+  public get Name(): string { return this.name }
+  public set Name(value: string) {
+    if(this.name === value) return
+    this.name = value
+  }
+
+  public sayHi(): void {
+    this.result = `Hello ${this.Name}`
+  }
+
+  public sayBye(): void {
+    this.result = `Goodbye ${this.Name}`
+  }
+
+  public say(something: string): void {
+    this.result = `Say ${something}`
+  }
+
+  change() {
+    this.visible = !this.visible
+    this.esthetic.error = !this.esthetic.error
+    this.esthetic.important = !this.esthetic.important
+  }
+
+  public calculate(a: number, b: number): number { return a + b }
+
+  public add(province: string) {
+    const id = this.list[this.list.length - 1].id + 1
+    this.list.push({id, name: province})
+    this.idProvince = id
+  }
 
   private subscriber?: Unsubscribable;
 
