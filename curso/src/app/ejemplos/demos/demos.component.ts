e51876f466760dd20ecc9d14859e846adb2f3426
verosampedro <verosampedro@icloud.com>
2024-07-11 14:44:51 +0200

    updating pipes

--- curso/src/app/ejemplos/demos/demos.component.ts
+++ curso/src/app/ejemplos/demos/demos.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CapitalizePipe, ElipsisPipe, MyCoreModule } from '@my/core';
 import { Unsubscribable } from 'rxjs';
 import { NotificationService, NotificationType } from 'src/app/common-services';
 
 @Component({
   selector: 'app-demos',
   standalone: true,
-  imports: [CommonModule, FormsModule ],
+  imports: [CommonModule, FormsModule, ElipsisPipe, CapitalizePipe, ],
   templateUrl: './demos.component.html',
   styleUrl: './demos.component.css'
 })
@@ -65,9 +66,6 @@ export class DemosComponent implements OnInit, OnDestroy  {
 
   ngOnInit(): void {
     this.suscriptor = this.vm.Notificacion.subscribe(n => {
-      // if (n.Type !== NotificationType.error) { return; }
-      // window.alert(`Suscripción: ${n.Message}`);
-      // this.vm.remove(this.vm.Listado.length - 1);
     });
   }
   ngOnDestroy(): void {
