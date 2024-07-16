cbfc3f06964a28a4dc08599b5e5dd0ac26bee426
verosampedro <verosampedro@icloud.com>
2024-07-16 08:52:26 +0200

    login form for admin

--- curso/src/app/ejemplos/dashboard/dashboard.component.ts
+++ curso/src/app/ejemplos/dashboard/dashboard.component.ts
@@ -7,16 +7,18 @@ import { CommonModule } from '@angular/common';
 import { CalculadoraComponent } from '../calculadora/calculadora.component';
 import { FormularioComponent } from '../formulario/formulario.component';
 import { ContactosComponent } from 'src/app/contactos';
+import { LoginComponent } from 'src/app/security';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NotificationComponent, CommonModule, AjaxWaitComponent, ],
+  imports: [NotificationComponent, CommonModule, AjaxWaitComponent, LoginComponent, ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
   menu = [
+    
     { texto: 'HOME', icono: 'fa-solid fa-house', componente: HomeComponent },
     { texto: 'FORM', icono: 'fa-solid fa-chalkboard-user', componente: FormularioComponent},
     { texto: 'CONTACTS', icono: 'fa-solid fa-address-book', componente: ContactosComponent},
